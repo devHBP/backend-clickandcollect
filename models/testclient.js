0dcd5a4c9deae90cba36587f405006d05298dfed
Cécilia Bernard <hbp@macbook-air-de-cecilia-1.home>
2023-06-12 14:37:50 +0200

    :construction: test new models clients

---
+++ models/testclient.js
@@ -0,0 +1,70 @@
+const { DataTypes } = require('sequelize')
+const db = require('../db/db')
+const Store = require('./magasins')
+
+const Test = db.define('Client', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role:{
+        type:DataTypes.STRING,
+        allowNull : false,
+        defaultValue : 'client'
+        // rôles: admin, gestionnaire, employe, client
+      },
+      id_magasin: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: null, // Définit la valeur par défaut sur null
+      },
+      adresse:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      }, 
+      cp: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      ville: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      telephone:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      //ajout 
+      date_naissance: {
+        type: DataTypes.DATEONLY,
+        allowNull: true, // Si la date de naissance peut être nulle
+      },
+      commentaires:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      }
+      
+})
+Test.belongsTo(Store, { foreignKey: 'id_magasin' });
+module.exports = Test
