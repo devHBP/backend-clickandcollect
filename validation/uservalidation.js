ff50dc702c9bffe249e92c32a3ca01198e13994e
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-06-20 11:28:56 +0200

    :zap: update regex

--- validation/uservalidation.js
+++ validation/uservalidation.js
@@ -2,11 +2,11 @@ const Joi = require('joi')
 
 const userValidation = ( body) => {
     const userSchema = Joi.object({
-        firstname: Joi.string().regex(/^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/).min(3).max(30).required(),
-        lastname: Joi.string().regex(/^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/).min(3).max(30).required(),
+        firstname: Joi.string().regex(/^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/).min(2).max(30).required(),
+        lastname: Joi.string().regex(/^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/).min(2).max(30).required(),
         email: Joi.string().email().required(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-        id_magasin: Joi.number().allow(null), 
+        password: Joi.string().pattern(new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{5,30}$')).required(),
+        storeId: Joi.number().allow(null), 
     })
     return userSchema.validate(body)
 }
