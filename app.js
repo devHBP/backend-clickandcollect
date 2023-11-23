97534ae9b7eca1fed9ab801c0eab170bbfff40c3
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-11-23 12:28:33 +0100

    :wrench: test websocket

--- app.js
+++ app.js
@@ -1,73 +1,59 @@
-const express = require('express')
-const routes = require('./routes/routes')
-const db = require('./db/db')
-var cors = require('cors')
-const path = require('path')
+const express = require("express");
+const routes = require("./routes/routes");
+const db = require("./db/db");
+var cors = require("cors");
+const path = require("path");
 const auth = require("./middleware/auth");
 const NODEJS_PORT = process.env.NODEJS_PORT;
 const NODEJS_URL = process.env.NODEJS_URL;
-const socketIo = require('socket.io')
-const http = require('http');
+const WebSocket = require("ws");
+const bodyParser = require("body-parser");
 
+const app = express();
 
-const bodyParser = require('body-parser');
-
-const app = express()
-
-
-const server = http.createServer(app); 
-const io = socketIo(server); 
-
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-console.log(path.join(__dirname, 'public'))
+app.use(express.static(path.join(__dirname, "public")));
+// console.log(path.join(__dirname, 'public'))
 
 // Ici j'utilise un server Static pour les images (en local)
 //a voir par la suite en réel avec peut etre une dropbox ?
-// je pars des Images en local /Images vers => 
-app.use('/Images', express.static(path.join(__dirname, './Images')));
-console.log('dirname', __dirname)
+// je pars des Images en local /Images vers =>
+app.use("/Images", express.static(path.join(__dirname, "./Images")));
+// console.log('dirname', __dirname)
 // const chemin = path.join(__dirname, './Images')
 // console.log('test chemin',chemin )
 
-app.get('/resetPassword/:token', (req, res) => {
+app.get("/resetPassword/:token", (req, res) => {
   //console.log("Route resetPassword appelée avec le token:", req.params.token);
-  res.sendFile(path.join(__dirname, './fonctions/resetpassword.html'));
+  res.sendFile(path.join(__dirname, "./fonctions/resetpassword.html"));
 });
 
 //telechargement du catalogue
-app.get('/download', (req, res) => {
-  const file = path.resolve(__dirname, './public/Catalogue.pdf');
-  res.download(file);  
+app.get("/download", (req, res) => {
+  const file = path.resolve(__dirname, "./public/Catalogue.pdf");
+  res.download(file);
 });
 app.post("/welcome", auth, (req, res) => {
   res.status(200).send("Welcome 🙌 ");
 });
 
-
-app.use(routes)
-
-io.on('connection', (socket) => {
-  console.log('Un client est connecté');
-
-  socket.on('disconnect', () => {
-    console.log('Un client est déconnecté');
-  });
-});
+app.use(routes);
 
 db.sync()
-    .then(
-        console.log('connexion à la base de données'),
-        )
-    .catch(error => console.log(error))
-
-
+  .then(console.log("connexion à la base de données"))
+  .catch((error) => console.log(error));
 
 server.listen(NODEJS_PORT, () => {
   console.log(`Lancement serveur ${NODEJS_URL}:${NODEJS_PORT}`);
 });
 
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("WebSocket connection established.");
+  socket.on("message", (message) => {
+    console.log(`Reçu: ${message}`);
+  });
+});
