f0a8e81ebed5e204eb63b5c052c306f7aa236f9f
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-11-28 14:43:19 +0100

    :bug: bug token

--- controllers/ctrl.js
+++ controllers/ctrl.js
@@ -214,15 +214,16 @@ const verifyToken = (req, res, next) => {
 
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
-      if (err.name === 'TokenExpiredError') {
-        return res.status(401).send({ auth: false, message: 'Token expired.' });
-      }
-      return res.status(403).send({ auth: false, message: 'Failed to authenticate token.' });
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
     }
+
+    // if everything is good, save to request for use in other routes
     req.userId = decoded.id;
+
+    // call next to pass control to the next middleware/function
     next();
   });
-}
+};
 
 //modification du user
 const modifyUser = async (req, res) => {
