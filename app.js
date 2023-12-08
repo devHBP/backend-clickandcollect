52c6b9d31b5ce6941c29511f9fb9b817dbfc0ceb
Cécilia Bernard <hbp@air-de-cecilia.home>
2023-12-08 08:53:02 +0100

    :wrench: add config firebase

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ const path = require("path");
 const auth = require("./middleware/auth");
 const NODEJS_PORT = process.env.NODEJS_PORT;
 const NODEJS_URL = process.env.NODEJS_URL;
-// const WebSocket = require("ws");
 const bodyParser = require("body-parser");
+const admin = require('firebase-admin');
 
 const app = express();
+
+
+// Chemin vers votre fichier de configuration Firebase
+const serviceAccount = require('./firebase.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.use(cors());
 app.use(express.json());
@@ -41,8 +49,6 @@ app.post("/welcome", auth, (req, res) => {
 
 app.use(routes);
 
-//create websocket server
-// const server = require('http').createServer(app);
 
 db.sync()
   .then(console.log("connexion à la base de données"))
@@ -52,15 +58,4 @@ app.listen(NODEJS_PORT, () => {
   console.log(`Lancement serveur ${NODEJS_URL}:${NODEJS_PORT}`);
 });
 
-// const wss = new WebSocket.Server({ server });
 
-// wss.on("connection", (ws) => {
-//   console.log("A new client connected");
-//   ws.send('Welcome new client')
-
-//   //the server received message from any client
-//   ws.on("message", (message) => {
-//     console.log(`Reçu: ${message}`);
-//     ws.send('Message bien recu' + message)
-//   });
-// });
