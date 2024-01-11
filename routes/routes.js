2272ef75461410996bd1890978d63e89e3dda935
Cécilia Bernard <hbp@air-de-cecilia.home>
2024-01-11 22:07:56 +0100

    :bug: bug with callappi no ok yes

--- routes/routes.js
+++ routes/routes.js
@@ -6,8 +6,11 @@ const { addProduct, getAllProducts, getOneProduct, uploadImage, updateProduct, d
 const { addFamillyProduct, getAllFamillyProducts, getOneFamillyProduct, deleteFamillyProduct } = require('../controllers/famille_produits_ctrl')
 const { addStore, getAllStores, getOneStore, updateStore, getStoresByRole } = require('../controllers/stores_ctrl')
 const {addPromo, handleApplyDiscount, allDiscounts, deletePromo } = require('../controllers/promo_ctrl')
-const { getAllStocks, getStockByProduct, getUpdateStockAntigaspi, checkStockAntiGaspi , getUpdateStock} = require('../controllers/stock_ctrl')
-const { createSession, success, paiementStatus, createPaiement, cancel , back } = require('../controllers/payment_ctrl')
+// const { getAllStocks, getStockByProduct, getUpdateStockAntigaspi, checkStockAntiGaspi , getUpdateStock} = require('../controllers/stock_ctrl')
+const { getAllStocks, getStockByProduct, getUpdateStockAntigaspi} = require('../controllers/stock_ctrl')
+// const { createSession, success, paiementStatus, createPaiement, cancel , back } = require('../controllers/payment_ctrl')
+const { createSession, success, paiementStatus, createPaiement} = require('../controllers/payment_ctrl')
+
 const { createOrder, updateStatusOrder, allOrders, deleteOneOrder, ordersOfUser, updateOrder,getOrderProducts , cancelOrder , 
     productsWithFormuleForOrder, ordersOfUserWithProducts, createReview, getAllReviews, statusLastOrder, tableOrderProduct, updateViewStatus } = require('../controllers/order_ctrl')
 const {sendWelcomeEmail } = require('../controllers/emails/welcomeEmail')
@@ -97,8 +100,8 @@ router.delete('/deletepromocodes/:id', deletePromo)
 router.get('/allStocks', getAllStocks)
 router.get('/getStockByProduct/:productId', getStockByProduct)
 router.put('/getUpdateStockAntigaspi', getUpdateStockAntigaspi)
-router.put('/getUpdateStock', getUpdateStock)
-router.post('/checkStockAntiGaspi', checkStockAntiGaspi)
+// router.put('/getUpdateStock', getUpdateStock)
+// router.post('/checkStockAntiGaspi', checkStockAntiGaspi)
 
 //ORDERS
 router.post('/createorder',createOrder ) // paiement sur place, seulement la commande ici
@@ -121,8 +124,8 @@ router.patch('/updateOrderPaidStatus', updateOrderPaidStatus);
 //PAYMENTS
 router.post('/checkout_session', createSession )
 router.get('/success', success)
-router.get('/cancel', cancel)
-router.get('/back', back)
+// router.get('/cancel', cancel)
+// router.get('/back', back)
 router.get('/paiementStatus', paiementStatus)
 router.post('/createPaiement', createPaiement) //paiement sur place, seuleulement le paiement ici
 
