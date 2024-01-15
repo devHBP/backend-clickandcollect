123e87e6adb1a04b5b9a682cd55b95de9adc71a3
Cécilia Bernard <hbp@air-de-cecilia.home>
2024-01-15 09:52:20 +0100

    :wrench: update url paiement in .env

--- controllers/payment_ctrl.js
+++ controllers/payment_ctrl.js
@@ -1,138 +1,76 @@
-const TestPaymentsV2 = require('../models/BDD/Payments')
-require('dotenv').config();
+const TestPaymentsV2 = require("../models/BDD/Payments");
+require("dotenv").config();
 
-
-const stripe = require('stripe')(process.env.STRIPE_KEY_PRIVATE)
-
-// const createSession = async (req, res) => {
-   
-//     const role = req.body.orderInfo.userRole;
-//     console.log('req', req.body.orderInfo.cart)
-//     const lineItems = req.body.orderInfo.cart.map((item) => {
-//       const { libelle, prix, qty, prix_unitaire } = item;
-//       let adjustedPrice = prix || prix_unitaire;
-//       console.log('item', item)
-//       console.log('libelle', libelle)
-//       console.log('prix', adjustedPrice)
-
-//       if (role === 'SUNcollaborateur') {
-//         adjustedPrice *= 0.80;
-//      }
-//       return {
-//         price_data: {
-//           currency: 'eur',
-//           product_data: {
-//             name: libelle,
-//           },
-//           unit_amount: Math.round(parseFloat(adjustedPrice * 100)),
-//           //unit_amount: parseFloat(prix_unitaire) * 100, // Assurez-vous de convertir le prix en centimes
-//         },
-//         quantity: qty,
-//       };
-//     });
-//     console.log('lineItems', lineItems)
-
-//     // adresses pour ios ou android
-//     let success_url = 'http://localhost:8080/success';
-//     let cancel_url = 'http://localhost:8080/cancel';
-    
-//     if (req.body.platform === 'android' && req.body.isDev) {
-//         success_url = 'http://10.0.2.2:8080/success';
-//         cancel_url = 'http://10.0.2.2:8080/cancel';
-//     } else if (req.body.platform === 'ios' && req.body.isDev) {
-//       //test sur iphone
-//         success_url = 'http://192.168.1.16:8080/success';
-//         cancel_url = 'http://localhost:8080/cancel';
-//     }
-
-//     const session = await stripe.checkout.sessions.create({
-//       payment_method_types: ['card'],
-//       line_items: lineItems,
-//       mode: 'payment',
-//       //adresse URL deepLinkg
-//       success_url: success_url,
-//       cancel_url: cancel_url,
-//     });
-//      //console.log('response',res)
-//     const sessionId = session.id;
-//     //console.log("sessionId", sessionId)
-//     //  console.log(res)
-//       res.json({ id: session.id, session: session.url, lineItems });
-      
-// }
+const stripe = require("stripe")(process.env.STRIPE_KEY_PRIVATE);
 
 const createSession = async (req, res) => {
   try {
-      // Valider les entrées
-      if (!req.body.orderInfo || !req.body.orderInfo.cart) {
-          return res.status(400).json({ error: "Informations de commande manquantes." });
+    // Valider les entrées
+    if (!req.body.orderInfo || !req.body.orderInfo.cart) {
+      return res
+        .status(400)
+        .json({ error: "Informations de commande manquantes." });
+    }
+
+    const role = req.body.orderInfo.userRole;
+    console.log("req", req.body.orderInfo.cart);
+    const lineItems = req.body.orderInfo.cart.map((item) => {
+      const { libelle, prix, qty, prix_unitaire, antigaspi } = item;
+      let adjustedPrice = prix || prix_unitaire;
+      console.log("item", item);
+      console.log("libelle", libelle);
+      console.log("prix", adjustedPrice);
+
+      if (role === "SUNcollaborateur" && !antigaspi) {
+        adjustedPrice *= 0.8;
       }
+      return {
+        price_data: {
+          currency: "eur",
+          product_data: {
+            name: libelle,
+          },
+          unit_amount: Math.round(parseFloat(adjustedPrice * 100)),
+        },
+        quantity: qty,
+      };
+    });
 
-      const role = req.body.orderInfo.userRole;
-      console.log('req', req.body.orderInfo.cart);
-      const lineItems = req.body.orderInfo.cart.map((item) => {
-          const { libelle, prix, qty, prix_unitaire, antigaspi } = item;
-          let adjustedPrice = prix || prix_unitaire;
-          console.log('item', item);
-          console.log('libelle', libelle);
-          console.log('prix', adjustedPrice);
+    console.log("lineItems", lineItems);
 
-          if (role === 'SUNcollaborateur' && !antigaspi) {
-            adjustedPrice *= 0.80; 
-        }
-          return {
-              price_data: {
-                  currency: 'eur',
-                  product_data: {
-                      name: libelle,
-                  },
-                  unit_amount: Math.round(parseFloat(adjustedPrice * 100)),
-              },
-              quantity: qty,
-          };
-      });
+    let success_url = `${process.env.ADRESS_PREPROD}/success/`;
+    let cancel_url = `${process.env.ADRESS_PREPROD}/cancel/`;
 
-      console.log('lineItems', lineItems);
-     
+    // if (req.body.platform === 'android' && req.body.isDev) {
+    //     success_url = 'http://10.0.2.2:8080/success';
+    //     cancel_url = 'http://10.0.2.2:8080/cancel';
+    // } else if (req.body.platform === 'ios' && req.body.isDev) {
+    //     success_url = 'http://192.168.1.16:8080/success';
+    //     cancel_url = 'http://localhost:8080/cancel';
+    // }
 
-      let success_url = `http://127.0.0.1:8080/success/`;
-      let cancel_url = `http://127.0.0.1:8080/cancel/`;
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: success_url,
+      cancel_url: cancel_url,
+    });
 
-      //let cancel_url = `https://preprod.lepaindujour.io/cancel/`;
-      // let success_url = `http://localhost:8080/success`;
-      
-
-      // if (req.body.platform === 'android' && req.body.isDev) {
-      //     success_url = 'http://10.0.2.2:8080/success';
-      //     cancel_url = 'http://10.0.2.2:8080/cancel';
-      // } else if (req.body.platform === 'ios' && req.body.isDev) {
-      //     success_url = 'http://192.168.1.16:8080/success';
-      //     cancel_url = 'http://localhost:8080/cancel';
-      // }
-
-      const session = await stripe.checkout.sessions.create({
-          payment_method_types: ['card'],
-          line_items: lineItems,
-          mode: 'payment',
-          success_url: success_url,
-          cancel_url: cancel_url,
-      });
-
-      res.json({ id: session.id, session: session.url, lineItems });
-
+    res.json({ id: session.id, session: session.url, lineItems });
   } catch (error) {
-      console.error('Erreur lors de la création de la session:', error);
-      res.status(500).json({ error: "Erreur lors de la création de la session." });
+    console.error("Erreur lors de la création de la session:", error);
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la création de la session." });
   }
-}
-
-
+};
 
 const success = (req, res) => {
-    // res.json({ message: 'Payment successful' });
-    // console.log('req', req)
-   console.log('paiement ok')
-   res.send(`
+  // res.json({ message: 'Payment successful' });
+  // console.log('req', req)
+  console.log("paiement ok");
+  res.send(`
    <html>
      <body>
       <div style="display: flex; align-items: center; justify-content: center; height: 100vh;">
@@ -146,13 +84,13 @@ const success = (req, res) => {
      </body>
    </html>
  `);
-}
+};
 
 const cancel = (req, res) => {
   // res.json({ message: 'Payment successful' });
   // console.log('req', req)
- console.log('paiement annulé backend')
- res.send(`
+  console.log("paiement annulé backend");
+  res.send(`
  <html>
    <body>
     <div style="display: flex; align-items: center; justify-content: center; height: 100vh;">
@@ -165,13 +103,13 @@ const cancel = (req, res) => {
    </body>
  </html>
 `);
-}
+};
 
 const back = (req, res) => {
   // res.json({ message: 'Payment successful' });
   // console.log('req', req)
- console.log('paiement annulé')
- res.send(`
+  console.log("paiement annulé");
+  res.send(`
  <html>
    <body>
     <div style="display: flex; align-items: center; justify-content: center; height: 100vh;">
@@ -184,7 +122,7 @@ const back = (req, res) => {
    </body>
  </html>
 `);
-}
+};
 
 const paiementStatus = async (req, res) => {
   try {
@@ -192,45 +130,61 @@ const paiementStatus = async (req, res) => {
 
     // Obtenez la session de paiement à partir de l'ID de session
     const session = await stripe.checkout.sessions.retrieve(sessionId);
-    console.log('session', session)
+    console.log("session", session);
     // Vérifiez l'état du paiement dans la session
     const paymentStatus = session.payment_status;
-    console.log('paiment status', paymentStatus)
+    console.log("paiment status", paymentStatus);
     const paymentId = session.payment_intent;
-    console.log('paiment id', paymentId)
+    console.log("paiment id", paymentId);
     const method = session.payment_method_types[0];
-    console.log('méthode de paiement', method); 
+    console.log("méthode de paiement", method);
 
-    if (session.payment_status = "unpaid"){
-      console.log('page d annulation ici')
+    if ((session.payment_status = "unpaid")) {
+      console.log("page d annulation ici");
     }
-    res.json({ status: paymentStatus, transactionId: paymentId, method: method });
+    res.json({
+      status: paymentStatus,
+      transactionId: paymentId,
+      method: method,
+    });
     //rajouter l'idpaiement
   } catch (error) {
-    console.error('Erreur lors de la vérification de l\'état du paiement - erreur ici :', error);
-    res.status(500).json({ error: 'Erreur lors de la vérification de l\'état du paiement- ICI' });
+    console.error(
+      "Erreur lors de la vérification de l'état du paiement - erreur ici :",
+      error
+    );
+    res.status(500).json({
+      error: "Erreur lors de la vérification de l'état du paiement- ICI",
+    });
   }
-}
-
+};
 
 const createPaiement = async (req, res) => {
   try {
     // Récupérer les données du corps de la requête
-    const { method, transactionId, status} = req.body;
+    const { method, transactionId, status } = req.body;
 
     // Créer le code promo dans la base de données
     const paiement = await TestPaymentsV2.create({
       method,
       transactionId,
-      status
+      status,
     });
 
-    res.status(201).json(paiement); 
+    res.status(201).json(paiement);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Une erreur est survenue lors de la création du paiement' });
+    res.status(500).json({
+      message: "Une erreur est survenue lors de la création du paiement",
+    });
   }
-}
+};
 
-
-module.exports = { createSession, success, paiementStatus, createPaiement, cancel, back }
+module.exports = {
+  createSession,
+  success,
+  paiementStatus,
+  createPaiement,
+  cancel,
+  back,
+};
