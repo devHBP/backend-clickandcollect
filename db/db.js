6201772fcac7f552789ae7a9ec9587f62f289a45
Cécilia Bernard <hbp@air-de-cecilia.home>
2024-02-02 15:00:46 +0100

    :wrench: update logs without select request

--- db/db.js
+++ db/db.js
@@ -8,6 +8,13 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     dialect:'mariadb',
-    host:'127.0.0.1'
+    host:'127.0.0.1',
+    logging: (query) => {
+        if (!query.includes("SELECT")) {
+          console.log(query);
+        }
+      },
 })
+
+
 module.exports = sequelize
