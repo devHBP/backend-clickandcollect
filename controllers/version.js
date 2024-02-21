e7b0f96d5764a7338f6ac8eaf0cb11e41ee3b33e
Cécilia Bernard <hbp@air-de-cecilia.home>
2024-02-21 09:42:08 +0100

    :wrench: update fonction for check hour antigaspi

--- controllers/version.js
+++ controllers/version.js
@@ -9,4 +9,28 @@ const status = (req, res) => {
   res.json({ maintenanceMode });
 };
 
-module.exports = { versionApp, status };
+const checkAntiGaspi = (req, res) => {
+
+  let now;
+  // Vérifier si un paramètre de requête `testTime` est fourni et l'utiliser comme heure actuelle pour le test
+  // heure simulée pour le test Jest
+  if (req.query.testTime) {
+    now = new Date(req.query.testTime);
+  } else {
+    now = new Date();
+  }
+  console.log('now', now)
+  const hour = now.getHours();
+
+  // L'offre est accessible à partir de 21h
+  if (hour >= 21 && hour < 24) {
+    res.json({ accessible: true });
+  } else {
+    res.json({
+      accessible: false,
+      message: "L'offre n'est pas encore disponible",
+    });
+  }
+};
+
+module.exports = { versionApp, status, checkAntiGaspi };
