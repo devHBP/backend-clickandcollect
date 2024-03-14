583d16130d9a176771d9a5980b1bab0aaaa7c27c
Cécilia Bernard <hbp@air-de-cecilia.home>
2024-03-14 11:12:47 +0100

    :wrench: update versionApp in bdd

--- controllers/version.js
+++ controllers/version.js
@@ -1,7 +1,6 @@
 const APP_VERSION = process.env.VERSION_STORE;
 const Users = require("../models/BDD/Users.js");
 
-
 const versionApp = async (req, res) => {
   res.json({ version: APP_VERSION });
 };
@@ -12,7 +11,6 @@ const status = (req, res) => {
 };
 
 const checkAntiGaspi = (req, res) => {
-
   let now;
   // Vérifier si un paramètre de requête `testTime` est fourni et l'utiliser comme heure actuelle pour le test
   // heure simulée pour le test Jest
@@ -21,7 +19,7 @@ const checkAntiGaspi = (req, res) => {
   } else {
     now = new Date();
   }
-  console.log('now', now)
+  // console.log("now", now);
   const hour = now.getHours();
 
   // L'offre est accessible à partir de 21h
@@ -34,15 +32,49 @@ const checkAntiGaspi = (req, res) => {
     });
   }
 };
+const getUserVersion = async (req, res) => {
+  // console.log(req.query);
+
+  const userId = req.query.userId;
+
+  if (!userId) {
+    return res.status(400).send({ message: "Le paramètre userId est requis." });
+  }
+
+  try {
+    const user = await Users.findOne({ where: { userId: userId } });
+
+    if (!user) {
+      return res.status(404).send({ message: "Utilisateur non trouvé." });
+    }
+
+    const versionApp = user.versionApp;
+
+    res.send({ versionApp: versionApp });
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération de la version de l'application pour l'utilisateur:",
+      error
+    );
+    res
+      .status(500)
+      .send({
+        message:
+          "Erreur lors de la récupération de la version de l'application.",
+      });
+  }
+};
 
 const updateVersion = async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
-  const userId = req.body.userId; 
-  const versionApp = req.body.versionApp; 
+  const userId = req.body.userId;
+  const versionApp = req.body.versionApp;
 
-  if (!userId ) {
-    return res.status(400).send({ message: "Les données userId et versionApp sont requises." });
+  if (!userId) {
+    return res
+      .status(400)
+      .send({ message: "Les données userId et versionApp sont requises." });
   }
 
   try {
@@ -54,11 +86,27 @@ const updateVersion = async (req, res) => {
 
     await user.update({ versionApp: versionApp });
 
-    res.send({ message: `Version de l'application mise à jour avec succès: ${versionApp}.` });
+    res.send({
+      message: `Version de l'application mise à jour avec succès: ${versionApp}. pour le user ${userId}`,
+    });
   } catch (error) {
-    console.error('Erreur lors de la mise à jour de la version de l\'application:', error);
-    res.status(500).send({ message: "Erreur lors de la mise à jour de la version de l'application." });
+    console.error(
+      "Erreur lors de la mise à jour de la version de l'application:",
+      error
+    );
+    res
+      .status(500)
+      .send({
+        message:
+          "Erreur lors de la mise à jour de la version de l'application.",
+      });
   }
-}
+};
 
-module.exports = { versionApp, status, checkAntiGaspi, updateVersion };
+module.exports = {
+  versionApp,
+  status,
+  checkAntiGaspi,
+  updateVersion,
+  getUserVersion,
+};
