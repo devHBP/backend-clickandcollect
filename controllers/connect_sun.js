ce7cb8713fedd3cd518905f3f8d5e30ab1b219ce
Cécilia Bernard <hbp@air-de-cecilia.home>
2024-03-27 14:41:00 +0100

    :wrench: update cancel link to sun

--- controllers/connect_sun.js
+++ controllers/connect_sun.js
@@ -6,10 +6,10 @@ const Users = require("../models/BDD/Users");
 
 const SUN_KEY = process.env.SUN_KEY;
 
-
 // Fonction pour valider le format de l'email
 function validateEmail(email) {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i;
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i;
   return re.test(String(email).toLowerCase());
 }
 
@@ -24,13 +24,18 @@ const receiveMsg = async (req, res) => {
 
   console.log("req", req.body.data);
   try {
-    const data = req.body.data; 
+    const data = req.body.data;
     const email = data.email;
-    const idSUN = data.id
+    const idSUN = data.id;
 
     // Validation du format de l'email
     if (!validateEmail(email)) {
-      return res.status(400).send({ status: "Erreur", message: "Le format de l'email est invalide." });
+      return res
+        .status(400)
+        .send({
+          status: "Erreur",
+          message: "Le format de l'email est invalide.",
+        });
     }
 
     // Recherche de l'utilisateur par email
@@ -43,38 +48,34 @@ const receiveMsg = async (req, res) => {
 
     // si un user a deja un statusSun renseigné, je veux envoyé une reponse le precisant
     if (user.statusSUN) {
-      return res.status(409).send({ // Utilisation du code 409 pour indiquer un conflit
+      return res.status(409).send({
+        // Utilisation du code 409 pour indiquer un conflit
         status: "Conflit",
-        message: `L'utilisateur est déja connecté à un compte pdj ou en attente de l'être.`
+        message: `L'utilisateur est déja connecté à un compte pdj ou en attente de l'être.`,
       });
     }
 
     await user.update({
       statusSUN: "en attente",
-      idSUN: idSUN 
+      idSUN: idSUN,
     });
     // console.log(`Utilisateur trouvé:`, user);
-    res
-      .status(200)
-      .send({
-        status: "Succès",
-        message: "Message reçu et utilisateur trouvé avec succès.",
-        user,
-      });
+    res.status(200).send({
+      status: "Succès",
+      message: "Message reçu et utilisateur trouvé avec succès.",
+      user,
+    });
   } catch (error) {
     console.error("Erreur:", error);
-    return res
-      .status(500)
-      .send({
-        status: "Erreur",
-        message: "Erreur lors du traitement de la requête.",
-      });
+    return res.status(500).send({
+      status: "Erreur",
+      message: "Erreur lors du traitement de la requête.",
+    });
   }
 };
 
 // verification du statusSUN
 const getStatusSun = async (req, res) => {
-
   const userId = req.params.userId;
   if (!userId) {
     return res.status(400).send({ message: "Le paramètre userId est requis." });
@@ -91,47 +92,37 @@ const getStatusSun = async (req, res) => {
 
     res.send({ statusSUN: statusSUN });
   } catch (error) {
-    console.error(
-      "Erreur lors de la récupération du statusSUN",
-      error
-    );
-    res
-      .status(500)
-      .send({
-        message:
-          "Erreur lors de la vérification.",
-      });
+    console.error("Erreur lors de la récupération du statusSUN", error);
+    res.status(500).send({
+      message: "Erreur lors de la vérification.",
+    });
   }
 };
 
 // envoi la confirmation de link vers sun (sun -> pdj)
 const sendConfirmLink = async (req, res) => {
-
   const { userId, idSUN } = req.body;
-
-
 
   try {
     const apiUrl = process.env.CONFIRM_LINK_SUN;
     const response = await axios.post(apiUrl, {
-      userId, 
-      idSUN, 
+      userId,
+      idSUN,
     });
 
     const user = await Users.findOne({ where: { userId: userId } });
 
-    if (response.data.status === 'success') { 
+    if (response.data.status === "success") {
       console.log("Confirmé");
 
       await user.update({
         statusSUN: "confirmé",
       });
     }
-    if (response.data.status === 'error') { 
+    if (response.data.status === "error") {
       console.log("erreur de data");
       // prevenir user de l'erreur
     }
-
   } catch (error) {
     console.error(
       "Erreur lors de l'envoi du message à l'API externe:",
@@ -141,40 +132,44 @@ const sendConfirmLink = async (req, res) => {
   }
 };
 
-
 // annulation (refus) du link sun
 const sendCancelLink = async (req, res) => {
-  const userId = req.params.userId;
-
-  if (!userId) {
-    return res.status(400).send({ message: "Le paramètre userId est requis." });
-  }
+  const { idSUN, userId } = req.body;
 
   try {
+    const apiUrl = process.env.CANCEL_LINK_SUN;
+    const response = await axios.post(apiUrl, {
+      idSUN,
+    });
+
     const user = await Users.findOne({ where: { userId: userId } });
 
-    if (!user) {
-      return res.status(404).send({ message: "Utilisateur non trouvé." });
+    if (response.data.status === "success") {
+
+      await user.update({
+        statusSUN: null,
+        idSUN: null,
+      });
+
+      console.log("Confirmé");
+
     }
-
-    await user.update({
-      statusSUN: null,
-      idSUN: null
-    });
-
-    res.send({ message: "Le statusSUN et l'idSUN ont été réinitialisés avec succès." });
+    if (response.data.status === "error") {
+      console.log("erreur de data");
+      // prevenir user de l'erreur
+    }
   } catch (error) {
-    console.error("Erreur lors de la réinitialisation du statusSUN et de l'idSUN", error);
-    res.status(500).send({
-      message: "Erreur lors de la réinitialisation."
-    });
+    console.error(
+      "Erreur lors de l'envoi du message à l'API externe:",
+      error.response ? error.response.data : error.message
+    );
+    return null;
   }
 };
-
 
 module.exports = {
   sendConfirmLink,
   receiveMsg,
   getStatusSun,
-  sendCancelLink
+  sendCancelLink,
 };
